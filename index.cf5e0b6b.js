7945ebf9b03e85f6a7bf990cb15b7249780e99df
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-06-12 18:09:30 +0200

    make build

---
+++ index.cf5e0b6b.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var r=e.querySelectorAll("th, td"),t=r[1].cloneNode(!0);e.insertBefore(t,r[r.length-1])});
+//# sourceMappingURL=index.cf5e0b6b.js.map
